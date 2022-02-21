38a1ccdbc993d3dabaf1af7d4ea158112068767b
tannie-123 <69163432+tannie-123@users.noreply.github.com>
2022-02-21 09:23:04 +0200

    Update ConfirmBooking.js

--- Screens/ConfirmBooking.js
+++ Screens/ConfirmBooking.js
@@ -15,25 +15,13 @@ const navigation = useNavigation();
         <Image style={style.bed4} source={require("./../assets/images/bed4.jpg")} >
                 </Image>
             <SafeAreaView style={style.container}>
-            {/* <ScrollView style={{width:'100%'}}
-                 horizontal={true}> */}
-               
-                {/* <Image style={style.bed4} source={require("./../assets/images/bed3.jpg")} >
-                </Image> */}
+            
                    <View style={style.btn}>
         <View style={{flex:1,marginBottom:20,justifyContent: 'flex-end',width:300,height:50 }}>
        <Button onPress={()=> navigation.navigate("Payment")} title='Book' color={'#E3AC1E'} ></Button>
        </View>
         </View> 
-        <View style={{flex:1,marginBottom:20,justifyContent: 'flex-end',width:300,height:50 }}>
-       <Button onPress={()=> navigation.navigate("Home")} title='Cancel' color={'#E3AC1E'} ></Button>
-        
-        {/* <View style={style.btn}> */}
        
-       {/* </View> */}
-        </View>     
-
-                {/* </ScrollView> */}
             </SafeAreaView>
         </>
     )
@@ -45,32 +33,7 @@ const style = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    // head: {
-    //     alignItems: 'center',
-    //     fontSize: 25,
-    //     color: '#00BFFF',
-    //     fontWeight: 'bold',
-    // },
-    // buttonTitle: {
-    //     color: "#E3AC1E",
-    //     fontWeight: 'bold',
-
-    // },
-    // inside: {
-    //     width: '100%',
-    //     borderBottomWidth: 1,
-    //     margin: 5
-    // },
-    // inputBox: {
-    //     flexDirection: 'row',
-    //     backgroundColor: '#FAF1A4',
-    //     //color: '#FAF1A4',
-    //     elevation: 3,
-    //     width: '80%',
-    //     height: 40,
-    //     margin: 5,
-    //     borderRadius:5, 
-    // },
+    
      bed4: {
     height: 400,
     borderBottomRightRadius: 40,
